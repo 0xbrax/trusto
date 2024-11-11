7fb2320a9bdf970b70ea8188e89f3da141a52144
0xbrax <marco.braccini@live.it>
2024-11-11 13:37:15 +1000

    v2 migration

--- components/custom/menubar.tsx
+++ components/custom/menubar.tsx
@@ -6,8 +6,8 @@ export default function Menubar() {
 
     return (
         <nav id="menubar"
-             className="w-full h-[4rem] absolute z-[5000] top-0 left-0 px-4 flex justify-center items-center bg-gradient-to-b from-primary-color/25 to-transparent">
-            <Link href="/" className="font-bold text-2xl">
+             className="w-full h-[4rem] absolute z-[5000] top-0 left-0 px-4 flex items-center bg-gradient-to-b from-primary-color/25 to-transparent">
+            <Link href="/" className="font-bold text-xl">
                 <span className="text-primary-color">Trust</span><span className="text-secondary-color">o</span>
             </Link>
         </nav>
